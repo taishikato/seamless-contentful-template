88617cfb6fe8d5be599bf6862a56851ab820822f
taishikato <taishi.k0903@gmail.com>
2019-11-21 12:43:40 +0700

    fix generateConfig.sh

--- generateConfig.sh
+++ generateConfig.sh
@@ -1,2 +1,2 @@
 # touch .contentful.json
-echo '{"CTF_PERSON_ID": '${1}', "CTF_BLOG_POST_TYPE_ID": '${2}', "CTF_SPACE_ID": '${3}', "CTF_CDA_ACCESS_TOKEN": '${4}', "CTF_CMA_ACCESS_TOKEN": '${5}'}' > .contentful.json
+echo '{"CTF_PERSON_ID": "'${1}'", "CTF_BLOG_POST_TYPE_ID": "'${2}'", "CTF_SPACE_ID": "'${3}'", "CTF_CDA_ACCESS_TOKEN": "'${4}'", "CTF_CMA_ACCESS_TOKEN": "'${5}'"}' > .contentful.json
